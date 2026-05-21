d3bbe5dcadf21ff64a2d13e0882e1223df13d47e
Maelle Allanic <52584285+maelleallanic@users.noreply.github.com>
2026-05-21 15:49:22 -0700

    add build step and serve frontend from express

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+// serve react frontend
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
